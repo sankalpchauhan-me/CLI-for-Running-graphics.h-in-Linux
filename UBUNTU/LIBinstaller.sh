cdd4faea9c39b8f41c4cdb9bbdd30ada1b668361
Sankalp Chauhan <sankalpchauhan.me@gmail.com>
2018-08-26 20:24:56 +0530

    Update LIBinstaller.sh
    Adding Guile 2.0 istead of 1.8 as guile 1.8 is depreceated in Ubuntu 18.04

--- UBUNTU/LIBinstaller.sh
+++ UBUNTU/LIBinstaller.sh
@@ -22,7 +22,9 @@ echo Essentials Installed Proceeding To Install Required Libraries
 echo 
 
 # Second Command
-sudo apt-get install libsdl-image1.2 libsdl-image1.2-dev guile-1.8   guile-1.8-dev libsdl1.2debian libart-2.0-dev libaudiofile-dev   libesd0-dev libdirectfb-dev libdirectfb-extra libfreetype6-dev   libxext-dev x11proto-xext-dev libfreetype6 libaa1 libaa1-dev   libslang2-dev libasound2 libasound2-dev
+#sudo apt-get install libsdl-image1.2 libsdl-image1.2-dev guile-1.8   guile-1.8-dev libsdl1.2debian libart-2.0-dev libaudiofile-dev   libesd0-dev libdirectfb-dev libdirectfb-extra libfreetype6-dev   libxext-dev x11proto-xext-dev libfreetype6 libaa1 libaa1-dev   libslang2-dev libasound2 libasound2-dev
+# Adding Guile 2.0 istead of 1.8 as guile 1.8 is depreceated in Ubuntu 18.04 (bionic beever)
+sudo apt-get install libsdl-image1.2 libsdl-image1.2-dev guile-2.0   guile-2.0-dev libsdl1.2debian libart-2.0-dev libaudiofile-dev   libesd0-dev libdirectfb-dev libdirectfb-extra libfreetype6-dev   libxext-dev x11proto-xext-dev libfreetype6 libaa1 libaa1-dev   libslang2-dev libasound2 libasound2-dev
 # END
 
 echo 
